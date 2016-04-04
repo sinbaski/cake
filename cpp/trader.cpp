0335f3f061932bc8de5af43e200f970bbdaac1e0
Xie Xiaolei <xie.xiaolei@gmail.com>
2016-04-04 21:52:38 +0200

    The program compiles now.

--- cpp/trader.cpp
+++ cpp/trader.cpp
@@ -1,94 +1,101 @@
 #include "trader.hpp"
+#include <iterator>
 #include <algorithm>
 
+using namespace std;
 
-trader::trader(float cash, int time_frame, const vector<float>& data)
+trader::trader(float cash, int time_frame, const vector<float>& data) throw()
 {
-  float z;
-  ForwardIterator it1, it2, it3;
+	float z;
+	vector<float>::const_iterator it1;
+	vector<float>::iterator it2;
+	vector<float>::const_iterator it3;
 
-  if (data.size() < 2 * time_frame - 1) {
-    throw trader_exception("length of data is less than twice the time frame.");
-  }
-  this->cash = cash;
-  this->time_frame = time_frame;
+	if (data.size() < (unsigned int)2 * time_frame - 1) {
+		throw trader_exception("length of data is less than "
+				       "twice the time frame.");
+	}
+	this->cash = cash;
+	this->time_frame = time_frame;
 
-  average[0] = accumulate(data.end() - 2*time_frame + 1,
-			  data.end() - time_frame, 0, [](float x, float y) {
-			    return x + y/time_frame;
-			  });
-  z = 0;
-  for ((it3 = data.end() - 2*time_frame + 1,
-	it1 = data.end() - time_frame,
-	it2 = average.begin());
-       it1 != data.end();
-       it1.next(), it2.next(), it3.next()) {
-    float x;
-    *it2 += *it1/time_frame - z;
-    z = *it3/time_frame;
-    x = *it1 - *it2;
-    upper_bound = max(x, upper_bound);
-    lower_bound = min(x, lower_bound);
-  }
-  this->data = vector<float>(data.end() - time_frame, data.end());
-}
-
-trader::trader(const trader& clone)
-{
-  this->data = clone.data;
-  this->time_frame = clone.time_frame;
-  this->upper_bound = clone.upper_bound;
-  this->lower_bound = clone.lower_bound;
-  this->upper_margin = clone.upper_margin;
-  this->lower_margin = clone.lower_margin;
-  this->average = clone.average;
-
-  this->trade = &clone.trade;
-  this->mutate = &clone.mutate;
+	average[0] = accumulate(data.end() - 2*time_frame + 1,
+				data.end() - time_frame, 0,
+				[&time_frame](float x, float y) {
+					return x + y/time_frame;
+				});
+	z = 0;
+	for ((it3 = data.end() - 2*time_frame + 1,
+	      it1 = data.end() - time_frame,
+	      it2 = average.begin());
+	     it1 != data.end();
+	     it1++, it2++, it3++) {
+		float x;
+		*it2 += *it1/time_frame - z;
+		z = *it3/time_frame;
+		x = *it1 - *it2;
+		upper_bound = max(x, upper_bound);
+		lower_bound = min(x, lower_bound);
+	}
+	this->data = vector<float>(data.end() - time_frame, data.end());
 }
 
 trader& trader::operator= (const trader& clone)
 {
-  return trader(clone);
+	this->data = clone.data;
+	this->time_frame = clone.time_frame;
+	this->upper_bound = clone.upper_bound;
+	this->lower_bound = clone.lower_bound;
+	this->unit_upper_margin = clone.unit_upper_margin;
+	this->unit_lower_margin = clone.unit_lower_margin;
+	this->average = clone.average;
+	return *this;
+}
+
+trader::trader(const trader& clone)
+{
+	operator= (clone);
 }
 
 contrararian::contrararian(float cash, int time_frame, const vector<float>& data)
-  :trader(cash, time_frame, data)
+	throw()
+	:trader(cash, time_frame, data)
 {
 }
 
-float contrararian::calculate_quantity(float new_price, transaction::transaction_t type)
+float contrararian::calculate_quantity(
+	float new_price,
+	transaction::transaction_t type)
 {
-  if (type == transaction::buy)
-    return cash/new_price/2;
-  else
-    return holding/2;
+	if (type == transaction::buy)
+		return cash/new_price/2;
+	else
+		return holding/2;
 }
 
-void contrararian::trade(float new_price, vector<transaction>& action)
+void contrararian::trade(float new_price, transaction& action)
 {
-  action->asset = asset;
+	action.asset = this->asset;
 
-  if (new_price > upper_bound * (1 + unit_upper_margin * time_frame)) {
-    action.type = transaction::sell;
-    action.quantity = calculate_quantity(new_price);
-    action.price = new_price;
-    cash += action.quantity * action.price;
-    holding -= action.quantity;
-  } else if (new_price < lower_bound * (1 + unit_lower_margin * time_frame)) {
-    action.type = transaction::buy;
-    action.quantity = calculate_quantity(new_price);
-    action.price = new_price;
-    holding += action.quantity;
-    cash -= action.quantity * action.price;
-  }
+	if (new_price > upper_bound * (1 + unit_upper_margin * time_frame)) {
+		action.type = transaction::sell;
+		action.quantity = calculate_quantity(new_price, action.type);
+		action.price = new_price;
+		cash += action.quantity * action.price;
+		holding -= action.quantity;
+	} else if (new_price <
+		   lower_bound * (1 + unit_lower_margin * time_frame)) {
+		action.type = transaction::buy;
+		action.quantity = calculate_quantity(new_price, action.type);
+		action.price = new_price;
+		holding += action.quantity;
+		cash -= action.quantity * action.price;
+	}
 
-  /* Update my status */
-  upper_bound = max(upper_bound, new_price);
-  lower_bound = min(lower_bound, new_price);
-  average.erase(average.begin());
-  average.push_back(*average.end() + (new_price - data[0])/time_frame);
-  data.erase(data.begin());
-  data.push_back(new_price);
-
+	/* Update my status */
+	upper_bound = max(upper_bound, new_price);
+	lower_bound = min(lower_bound, new_price);
+	average.erase(average.begin());
+	average.push_back(*average.end() + (new_price - data[0])/time_frame);
+	data.erase(data.begin());
+	data.push_back(new_price);
 }
--- cpp/trader.hpp
+++ cpp/trader.hpp
@@ -1,6 +1,9 @@
 #ifndef trader_hpp
 #define trader_hpp
 
+#include <exception>
+#include <vector>
+
 using namespace std;
 
 // class newdata
@@ -13,64 +16,62 @@ using namespace std;
 class trader_exception : public exception
 {
 private:
-  const char *message;
+	const char *message;
 
 public:
-  trader_exception(const char *message) {
-    this->message = message;
-  };
+	trader_exception(const char *message) {
+		this->message = message;
+	};
 
-  virtual const char* what() const throw() {
-    return message;
-  };
-}
+	virtual const char* what() const throw() {
+		return message;
+	};
+};
 
 class transaction
 {
 public:
-  enum transaction_t {buy, sell};
+	enum transaction_t {buy, sell};
 
-  const char *asset;
-  float quantity;
-  float price;
-  transaction_t type;
+	const char *asset;
+	float quantity;
+	float price;
+	transaction_t type;
 };
-
-typedef void (*trade_fun)(float new_price, vector<transaction>* action);
 
 /**
  * Each trader trades only one asset.
  */
 class trader
 {
-private:
-  float holding = 0;
-  int time_frame;
-  float upper_bound = 0;
-  float lower_bound = 0;
-  float unit_upper_margin = 0;
-  float unit_lower_margin = 0;
-  vector<float> data;
-  vector<float> average;
+protected:
+	float holding = 0;
+	unsigned int time_frame;
+	float upper_bound = 0;
+	float lower_bound = 0;
+	float unit_upper_margin = 0;
+	float unit_lower_margin = 0;
+	vector<float> data;
+	vector<float> average;
 
 public:
-  const char *asset;
-  float cash = 1;
-  virtual trade_fun trade = 0;
-  virtual void mutate(void) = 0;
+	const char *asset;
+	float cash = 1;
+	virtual void trade(float new_price, transaction& action) = 0;
+	virtual void mutate(void) = 0;
 
-  trader(float cash, vector data) throw();
-  trader(const trader& clone, float cash);
-  trader& operator= (const trader& x);
-  ~trader(void);
+	trader(float cash, int time_frame, const vector<float>& data) throw();
+	trader(const trader& clone);
+	trader& operator= (const trader& x);
+	~trader(void);
 
-  float get_cash(void) {
-    return cash;
-  };
+	float get_cash(void) {
+		return cash;
+	};
 
-  float get_holding(void) {
-    return holding;
-  }
+	float get_holding(void) {
+		return holding;
+	}
 };
 
 class contrararian : public trader
@@ -78,9 +79,12 @@ class contrararian : public trader
 private:
 
 public:
-  contrararian(float cash, int time_frame, const vector<float>& data) throw();
-  float calculate_quantity(float new_price);
-  trade_fun trade;
+	contrararian(float cash, int time_frame, const vector<float>& data)
+		throw();
+	virtual float calculate_quantity(
+		float new_price,
+		transaction::transaction_t type);
+	virtual void trade(float new_price, transaction& action);
 };
 
 #endif
