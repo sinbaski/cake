5ae87c002621e8915973c0260dda6cca9b0f7b3f
Xie Xiaolei <xie.xiaolei@gmail.com>
2016-08-29 00:55:09 +0200

    Experimented with TA indices.

--- dreamcake.r
+++ dreamcake.r
@@ -1,18 +1,52 @@
 rm(list=ls());
+graphics.off();
 library("TTR");
 library("quantmod");
-source("~/hdrive/work/r/libxxie.r");
+source("common/libxxie.r");
 
-database = dbConnect(MySQL(), user='sinbaski', password='q1w2e3r4',
-                     dbname='avanza', host="localhost");
-results <- dbSendQuery(database,
-                       "select * from DAX where day between '2012-01-01' and '2015-05-27' order by day;");
-data <- fetch(results, n=-1);
-dbClearResult(results);
-dbDisconnect(database);
+getSymbols("DAX");
+n <- dim(DAX)[1];
+data <- DAX[(n-260):n, ];
+## plot(data[, "DAX.Close"]);
 
-##plot(1:length(data$closing), data$closing, type="l");
-candleChart(data, type="candlesticks");
-adx <- ADX(data[, c("high", "low", "closing")]);
-lines(1:length(data$closing), adx[, "DIp"], col="#00FF00");
-lines(1:length(data$closing), adx[, "DIn"], col="#FF0000");
+
+
+chartSeries(data,
+            theme=chartTheme('white'),
+            TA=c(addVo(),addBBands(),
+                 addMACD(), addCCI(),
+                 addRSI(), addOBV()
+                 ))  #add volume and Bollinger Bands from TTR
+
+X <- EMA(data[, "DAX.Volume"], n=12) - EMA(data[, "DAX.Volume"], n=26);
+Y <- EMA(X, n=9);
+## vo.macd <- xts(list(fast=X, slow=Y), order=time(X));
+
+addTA(X, on=NA,
+      legend="VoMACD",
+      col="#0000FF", type="l");
+addTA(Y, on=7,
+      col="#000000", type="l");
+
+## X11();
+## chartSeries(data,
+##             theme=chartTheme('white'),
+##             TA=c(addBBands(), addDPO(), addOBV()))
+
+## stol <- stoch(data[, c("DAX.High", "DAX.Low", "DAX.Close")]);
+
+## X11();
+## plot(stol[, "fastD"]);
+## lines(stol[, "slowD"], col="#FF0000")
+
+## I like the following
+## Stochastic Oscillator
+## Donchian channel
+
+## Have looked at the following
+## SMI stochastic momentum index
+## CMO (up - down)/(up + down)
+## RSI up/(up + down)
+
+## SAR Parabolic SAR
+
