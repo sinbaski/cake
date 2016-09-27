002896b4e92bf6b13fd683511a5a3858294b56a6
Xie Xiaolei <xie@math.ku.dk>
2016-09-27 19:47:17 +0200

    Implemented the strategy of generalized pair trading.

--- FX2.r
+++ FX2.r
@@ -1,3 +1,4 @@
+library("nortest");
 rm(list=ls());
 source("/home/lxb353/hdrive/work/r/libxxie.r");
 
@@ -60,26 +61,88 @@ names <- c(
     "USD"
     );
 
-ret <- getAssetReturns("2010-01-04", "2016-04-01",
-                       currencies, 1,
-                       "rate", "localhost");
+prices <- getAssetPrices("2010-01-04", "2016-04-01",
+                         currencies, 1,
+                         "rate", "localhost");
+ret <- diff(log(prices));
 n <- dim(ret)[1];
 p <- dim(ret)[2];
+h <- 20;
+m <- 3;
+lookback <- m*h;
+I <- 8;
+N <- 4;
+components <- 1:N;
+
+actions <- rep(0, n - lookback + 1);
+portfolio <- rep(NA, p);
+wealth <- 1;
+W <- rep(-1, n - lookback + 1);
+for (i in  lookback : n) {
+    if (i %% h == 0) {
+        R <- ret[(i - lookback + 1):i, ];
+        E <- eigen(cov(R));
+        Y <- R %*% E$vectors;
+        X <- R[, I];
+        model <- lm(X ~ Y[, 1:N]);
+        composition <- E$vectors[, 1:N] %*% model$coefficients[components + 1];
+    }
+    ## F <- ret[(i - h + 1):i, ] %*% E$vectors[, 1:N];
+    ## Z <- F %*% model$coefficients[2:5];
+    Z <- ret[(i - h + 1):i, ] %*% composition;
+    S1 <- cumsum(Z);
+    S2 <- cumsum(ret[(i - h + 1) : i, I]);
+    D <- S2 - S1;
+    dev <- sd(S2 - S1);
+
+    if (D[h] > 2 * dev && is.na(portfolio[1])) {
+        actions[i - lookback + 1] <- -1;
+
+        portfolio[I] <- -wealth / 2 / prices[(i+1), I];
+        portfolio[-I] <- rep(0, p-1);
+        portfolio <- portfolio + wealth / 2 * (composition/sum(abs(composition))/prices[i+1,]);
+        wealth <- wealth - drop(prices[i+1, ] %*% portfolio);
+
+    } else if (D[h] < -2 * dev && is.na(portfolio[1])) {
+        actions[i - lookback + 1] <- 1;
+
+        portfolio[I] <- wealth / 2 / prices[(i+1), I];
+        portfolio[-I] <- rep(0, p-1);
+        portfolio <- portfolio + (wealth / 2) * (-composition/sum(abs(composition))/prices[i+1,]);
+        wealth <- wealth - drop(prices[i+1, ] %*% portfolio);
+
+    } else if (!is.na(portfolio[1])) {
+        k <- max(which(actions != 0));
+        if (actions[k] == 1 && D[h] > 0) {
+            actions[i - lookback + 1] <- -1;
+            wealth <- wealth + sum(portfolio * prices[i+1, ]);
+            portfolio[1] <- NA;
+        } else if (actions[k] == -1 && D[h] < 0) {
+            actions[i - lookback + 1] <- 1;
+            wealth <- wealth + sum(portfolio * prices[i+1, ]);
+            portfolio[1] <- NA;
+        }
+    }
+    W[i - lookback + 1] <- wealth;
+}
+J <- which(actions != 0);
+## l <- n - lookback + 1;
+## plot(1:l, cumsum(N), type="l");
 
 ## E <- eigen(cov(ret));
 ## D <- eigen(cov(abs(ret)));
 ## F <- eigen(cov(ret^2));
 
-m <- floor(n/2);
-E <- eigen(cov(ret[1:m, ]));
-Y <- ret %*% E$vectors;
-X1 <- ret[1:m, 1];
-model <- lm(X1 ~ Y[(1:m), 1:4]);
-Z <- Y[(m+1):n, 1:4] %*% model$coefficients[2:5];
-S1 <- cumsum(Z);
-S2 <- cumsum(ret[(m+1):n, 1]);
-M <- lm(S2 ~ S1);
+## m <- floor(n/2);
+## E <- eigen(cov(ret[1:m, ]));
+## Y <- ret %*% E$vectors;
+## X1 <- ret[1:m, 1];
+## model <- lm(X1 ~ Y[(1:m), 1:N]);
+## Z <- Y[(m+1):n, 1:N] %*% model$coefficients[2:5];
+## S1 <- cumsum(Z);
+## S2 <- cumsum(ret[(m+1):n, 1]);
+## M <- lm(S2 ~ S1);
 
-plot((m+1):n, M$fitted.values, type="l", ylim=c(min(c(S1, S2)), max(c(S1, S2))));
-lines((m+1):n, S2, col="#0000FF");
+## plot((m+1):n, M$fitted.values, type="l", ylim=c(min(c(S1, S2)), max(c(S1, S2))));
+## lines((m+1):n, S2, col="#0000FF");
 
