d43a67f06e6005147ed9286bfbb163fdb7eb8977
Xie Xiaolei <xie.xiaolei@gmail.com>
2016-10-02 23:24:37 +0200

    Revised Risk management.

--- FX2.r
+++ FX2.r
@@ -79,24 +79,67 @@ actions <- rep(0, n - lookback + 1);
 portfolio <- rep(NA, p);
 wealth <- 1;
 W <- rep(-1, n - lookback + 1);
+estimate.fun <- function(model, X)
+{
+    if (model == "GARCH") {
+        M <- garchFit(
+            ~garch(1, 1), X, trace=FALSE,
+            cond.dist="norm",
+            include.shape=FALSE,
+            include.mean=TRUE,
+            include.delta=FALSE,
+            include.skew=FALSE
+        );
+    } else if (model == "ARCH") {
+        M <- garchFit(
+            ~garch(1, 0), data=X, trace=FALSE,
+            cond.dist="norm",
+            include.shape=FALSE,
+            include.mean=TRUE,
+            include.delta=FALSE,
+            include.skew=FALSE
+        );
+    } else {
+        M <- NULL;
+    }
+    return (M);
+}
+
 for (i in  (lookback + warmup) : n) {
     if ((i - warmup) %% h == 0) {
         R <- ret[(i - lookback + 1):i, ];
         E <- eigen(cov(R));
         Y <- R %*% E$vectors;
         X <- R[, I];
-        model <- lm(X ~ Y[, 1:N]);
-        composition <- E$vectors[, 1:N] %*% model$coefficients[components + 1];
+        model <- lm(X ~ Y[, 1:N] + 0);
+        composition <- E$vectors[, 1:N] %*% model$coefficients[components];
 
-        ## A GARCH(1, 1) model for each sequence. The innovations are correlated.
+        ## A GARCH(1, 1)/ARCH(1) model for each sequence. The innovations are correlated.
         garch11 <- {};
-        res <- matrix(NA, nrow=i, ncol=p);
+        garch.coef <- matrix(0, nrow=p, ncol=4);
+        inno <- matrix(NA, nrow=i, ncol=p);
+        chosen <- {};
         for (j in 1:p) {
-            M <- garchFit(~garch(1,1), data=(ret[1:i, j] - mean(ret[1]), trace=FALSE);
-            garch11 <- c(garch11, M);
-            res[, j] <- garch11[[j]]@residuals / garch11[[j]]$sigma.t;
+            M <- tryCatch({
+                estimate.fun("GARCH", ret[1:i, j])
+            },
+            error=function(err) {
+                estimate.fun("ARCH", ret[1:i, j])
+            },
+            warning=function(war){
+                estimate.fun("ARCH", ret[1:i, j])
+            }
+            );
+            if (! is.null(M)) {
+                garch11 <- c(garch11, M);
+                inno[, j] <- M@residuals / M@sigma.t;
+                inno[, j] <- inno[, j] - mean(inno[, j]);
+                inno[, j] <- inno[, j] / sd(inno[, j]);
+                garch.coef[j, 1:length(M@fit$coef)] <- M@fit$coef;
+                chosen <- union(chosen, j);
+            }
         }
-        C <- cor(res);
+        C <- cor(inno);
     }
     ## F <- ret[(i - h + 1):i, ] %*% E$vectors[, 1:N];
     ## Z <- F %*% model$coefficients[2:5];
@@ -104,7 +147,7 @@ for (i in  (lookback + warmup) : n) {
     S1 <- cumsum(Z);
     S2 <- cumsum(ret[(i - h + 1) : i, I]);
     D <- S2 - S1;
-    dev <- sd(S2 - S1);
+    dev <- sd(D);
 
     if (D[h] > 2 * dev && is.na(portfolio[1])) {
         actions[i - lookback + 1] <- -1;
