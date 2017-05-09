fd809e96d36b1e8719647434c9c42a6528b9f954
Xie Xiaolei <xie.xiaolei@gmail.com>
2017-05-09 16:26:08 +0200

    Added the document about profiting from high-low-closing-volume relations.

--- PriceDifferenceRegression.r
+++ PriceDifferenceRegression.r
@@ -1,4 +1,6 @@
 rm(list=ls())
+require(RMySQL);
+require(MTS);
 source("common/libxxie.r")
 
 database = dbConnect(MySQL(), user='sinbaski', password='q1w2e3r4',
@@ -7,7 +9,7 @@ database = dbConnect(MySQL(), user='sinbaski', password='q1w2e3r4',
 end.date <- '2015-03-07';
 len <- 25;
 results <- dbSendQuery(database,
-                       paste("select * from DO_US ",
+                       paste("select * from SP500 ",
                              "where day <= '", end.date, "' ",
                              "and day >= date_add('", end.date,
                              "', interval -", len, " week) ",
@@ -16,17 +18,10 @@ results <- dbSendQuery(database,
 records <- fetch(results);
 dbClearResult(results);
 
-h <- 5;
-num <- dim(records)[1];
-n <- num - h;
-
-
-P <- diff(log(records$closing));
-V <- diff(log(records$volume));
 X <- apply(log(records[, -1]), MARGIN=2, FUN=diff);
 
 results <- dbSendQuery(database,
-                       paste("select * from SP500 ",
+                       paste("select * from DJIA ",
                              "where day <= '", end.date, "' ",
                              "and day >= date_add('", end.date,
                              "', interval -", len, " week) ",
@@ -34,22 +29,45 @@ results <- dbSendQuery(database,
                        );
 index <- fetch(results);
 dbClearResult(results);
-
 Y <- apply(log(index[, -1]), MARGIN=2, FUN=diff);
+
+results <- dbSendQuery(database,
+                       paste("select * from AAPL_US ",
+                             "where day <= '", end.date, "' ",
+                             "and day >= date_add('", end.date,
+                             "', interval -", len, " week) ",
+                             "order by day")
+                       );
+records <- fetch(results);
+dbClearResult(results);
+Z <- apply(log(records[, -1]), MARGIN=2, FUN=diff);
+
+
 ## X <- cbind(log(records$high) - log(records$closing),
 ##            log(records$closing) - log(records$low));
 ## X <- cbind(X, log(records$volume));
 
-ccf(P, X[, 4], lag.max=20);
-ccf(P, Y[, 4], lag.max=20);
+pdf("~/cake/DJIA_HighLow_ccf.pdf")
+ccf(Y[, 1], Y[, 2], lag.max=20, col="red", lwd=2,
+    main=expression(ccf(bar(X), underline(X))),
+    xaxt="n",
+    xlab="DJIA",
+    ylab="correlation"
+    );
+axis(side=1, at=seq(from=-20, to=20, by=5));
+abline(v=-20:20, lty=3);
+dev.off();
 
 ## X <- as.matrix(records[1:(n-1), -1]);
-model <- lm(P ~ V);
-C <- coef(model);
-predictions <- C[1] * P
+## model <- lm(R ~ V);
+## C <- coef(model);
+## predictions <- C[1] * P
 
-predictions <- as.matrix(records[n:(n+h-1), -1]) %*% C[-1] + C[1];
-cbind(records$closing[(n+1):(n+h)], predictions);
+## predictions <- as.matrix(records[n:(n+h-1), -1]) %*% C[-1] + C[1];
+## cbind(records$closing[(n+1):(n+h)], predictions);
 
+model <- VARMA(Y, p=2, q=1, include.mean=F);
+M <- refVARMA(model, thres=qt(p=0.975, df=dim(Y)[1]-1));
+N <- refVARMA(model);
 dbDisconnect(database);
 
