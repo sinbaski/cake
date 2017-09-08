a291ea9cb09c4da40228c4f4bb8cd9138b72f3eb
Xie Xiaolei <xie.xiaolei@gmail.com>
2017-09-08 18:25:56 +0200

    Some work on alpha generation.

--- temp.sql
+++ temp.sql
@@ -1,12 +1,23 @@
 -- NDA_series_SEK_ST_SE
+
+-- drop table if exists Deviations;
+-- create table Deviations (
+--        day date primary key,
+--        -- closing decimal(7,2),
+--        price double,
+--        deviation real
+-- ) as
+-- select T2.day, T2.rate as price, (T2.rate - avg(T1.rate))/avg(T1.rate) as deviation from EUR_SEK_Rates as T2 join EUR_SEK_Rates as T1 on datediff(T2.day, T1.day) between 1 and 60 where T2.day between '2013-01-02' and '2014-09-30' group by T2.day;
+
 drop table if exists Deviations;
 create table Deviations (
        day date primary key,
-       -- closing decimal(7,2),
        price double,
        deviation real
 ) as
-select T2.day, T2.rate as price, (T2.rate - avg(T1.rate))/avg(T1.rate) as deviation from EUR_SEK_Rates as T2 join EUR_SEK_Rates as T1 on datediff(T2.day, T1.day) between 1 and 60 where T2.day between '2013-01-02' and '2014-09-30' group by T2.day;
+select T2.day, T2.closing as price,  log(T2.closing) - log(avg(T1.closing)) as deviation from KO_US as T2 join KO_US as T1 on datediff(T2.day, T1.day) between 1 and 60 where T2.day between '2013-01-02' and '2014-09-30' group by T2.day;
+
+
 
 -- drop function if exists padd;
 -- delimiter #
@@ -21,20 +32,20 @@ select T2.day, T2.rate as price, (T2.rate - avg(T1.rate))/avg(T1.rate) as deviat
 -- end#
 -- delimiter ;
 
-drop function if exists todo;
-delimiter #
-create function todo (x float) returns varchar(4)
-begin
-	if x < -0.03671877 then
-	   set @s = 'buy';
-	elseif x > 0.04061545  then
-	   set @s = 'sell';
-	else
-	   set @s = '';
-	end if;
-	return @s;
-end#
-delimiter ;
+-- drop function if exists todo;
+-- delimiter #
+-- create function todo (x float) returns varchar(4)
+-- begin
+-- 	if x < -0.03671877 then
+-- 	   set @s = 'buy';
+-- 	elseif x > 0.04061545  then
+-- 	   set @s = 'sell';
+-- 	else
+-- 	   set @s = '';
+-- 	end if;
+-- 	return @s;
+-- end#
+-- delimiter ;
 
 -- select day, closing, deviation, todo(deviation) from (
 -- select T2.day, T2.closing, (T2.closing - avg(T1.closing))/avg(T1.closing) as deviation from NDA_series_SEK_ST_SE as T2 join NDA_series_SEK_ST_SE as T1 on T2.day > T1.day and datediff(T2.day, T1.day) <= 60 where T2.day between '2014-10-01' and '2014-12-31' group by T2.day ) as tbl ;
--- test2.r
+++ test2.r
@@ -10,7 +10,7 @@ database = dbConnect(MySQL(), user='sinbaski', password='q1w2e3r4',
 
 ## stmt <- "select T2.day, T2.closing, (T2.closing - avg(T1.closing))/avg(T1.closing) as deviation from NDA_series_SEK_ST_SE as T2 join NDA_series_SEK_ST_SE as T1 on T2.day > T1.day and datediff(T2.day, T1.day) <= 60 where T2.day between '2014-04-01' and '2014-09-30' group by T2.day;";
 
-## stmt <- "select day, price, deviation from Deviations;";
+stmt <- "select day, price, deviation from Deviations;";
 
 results = dbSendQuery(
     database, stmt
@@ -21,10 +21,19 @@ dbClearResult(results);
 dbDisconnect(database);
 
 X = xts(D$deviation, order.by=as.Date(D$day));
-Y <- diff(D$deviation);
+plot(X);
 
-Tdist <- fitdistr(Y, "t");
-Ndist <- fitdistr(Y, "normal");
+Y <- diff(D$deviation);
+plot(xts(Y, order.by=as.Date(D$day[-1])))
+
+## Model X as an Ornstein Uhlenbeck process for simplicity
+## Use the sde package. Function dcOU gives the conditional density function of an OU process.
+## Even better: Y has heavy-tails. Model X as an Levy process?
+
+
+
+## Tdist <- fitdistr(Y, "t");
+## Ndist <- fitdistr(Y, "normal");
 
 
 
