e3d50291c39d8d3b6f14b8a016d1ab2fa7ad22f2
Xiaolei Xie <xiaolei.xie@atenvia.com>
2017-11-14 11:24:59 +0000

    Revised the market neutral strategy.

--- OU2.r
+++ OU2.r
@@ -11,77 +11,154 @@ database = dbConnect(MySQL(), user='sinbaski', password='q1w2e3r4',
                      dbname='market', host="localhost");
 
 
+## stmt <- paste(
+##     "select ko_daily.tm as tm,", 
+##     "ko_daily.closing as ko,",
+##     "ko_daily.high - ko_daily.low ko_range,",
+##     "spy_daily.closing as spy,",
+##     "spy_daily.high - spy_daily.low as spy_range,",
+##     "xlp_daily.closing as xlp,",
+##     "xlp_daily.high - xlp_daily.low as xlp_range",
+##     "from ko_daily join spy_daily join xlp_daily",
+##     "on ko_daily.tm = spy_daily.tm and ko_daily.tm = xlp_daily.tm;"
+## );
 stmt <- paste(
-    "select ko_daily.tm as tm,", 
+    "select ko_daily.tm as tm,",
     "ko_daily.closing as ko,",
-    "ko_daily.high - ko_daily.low ko_range,",
+    "pep_daily.closing as pep,",
     "spy_daily.closing as spy,",
-    "spy_daily.high - spy_daily.low as spy_range,",
-    "xlp_daily.closing as xlp,",
-    "xlp_daily.high - xlp_daily.low as xlp_range",
-    "from ko_daily join spy_daily join xlp_daily",
-    "on ko_daily.tm = spy_daily.tm and ko_daily.tm = xlp_daily.tm;"
+    "xlp_daily.closing as xlp",
+    "from ko_daily join pep_daily",
+    "join spy_daily",
+    "join xlp_daily",
+    "on ko_daily.tm = pep_daily.tm",
+    "and ko_daily.tm = spy_daily.tm",    
+    "and ko_daily.tm = xlp_daily.tm"    
 );
+
 results <- dbSendQuery(database, stmt);
 X <- fetch(results, n=-1);
 dbClearResult(results);
 dbDisconnect(database);
 
 n <- dim(X)[1];
+p <- dim(X)[2]-1;
 look.back <- 60;
 ma.t <- 10;
 action <- {};
 trades <- {};
 prob.trig <- 0.05;
-holding <- rep(0, 3);
-cash <- rep(0, n);
-wealth <- rep(0, n);
+holding <- matrix(NA, nrow=n, ncol=p);
+cash <- matrix(NA, nrow=n, ncol=p);
+wealth <- matrix(NA, nrow=n, ncol=p);
 position <- 0;
-for (t in (look.back + 1):(n-50)) {
-## for (t in (look.back + 1):73) {
-    model <- lm(ko~spy+xlp-1, data=X[(t-look.back):(t-1), c(2,4,6)]);
-    Y <- residuals(model);
-    EY <- EMA(Y, n=ma.t)
-    F <- ecdf(Y - EY);
-    value <- X[t, 2] - predict(model, newdata=X[t, c(4,6)], n.ahead=1);
-    ## prob <- F(value);
+trading.allowed <- NA;
+deviations <- rep(NA, n);
+ratio.hold <- matrix(NA, nrow=n, ncol=p);
+ratio.cal <- matrix(NA, nrow=n, ncol=p);
+for (t in (look.back + 1):n) {
+    ## for (t in (look.back + 1):73) {
+    period <- (t-look.back):(t-1);
+    prices <- X[period, ];
+    ## model.vol.ko <- garchFit(~garch(1,1), data=ret[, 1], trace=FALSE);
+    ## model.vol.spy <- garchFit(~garch(1,1), data=ret[, 2], trace=FALSE);
+    ## model.vol.xlp <- garchFit(~garch(1,1), data=ret[, 3], trace=FALSE);
+    ## sigma <- c(
+    ##     tail(model.vol.ko@sigma.t, 1),
+    ##     tail(model.vol.spy@sigma.t, 1),
+    ##     tail(model.vol.xlp@sigma.t, 1)
+    ## );
+    ## vol.params <- matrix(
+    ##     c(coef(model.vol.ko), coef(model.vol.spy), coef(model.vol.xlp)),
+    ##     ncol=4, nrow=3, byrow=TRUE
+    ## )[, 2:4];
+    ## newdata <- matrix(c(rep(1, 3), tail(ret, n=1), sigma), nrow=3, ncol=3,
+    ##                   byrow=FALSE);
+    ## vol.t <- sqrt(apply(vol.params * newdata^2, MARGIN=1, FUN=sum));
+    ## F.ko <- ecdf(model.vol.ko@sigma.t);
+    ## F.spy <- ecdf(model.vol.spy@sigma.t);
+    ## F.xlp <- ecdf(model.vol.xlp@sigma.t);
+    
+    ## trading.allowed <- F.ko(vol.t[1]) < vol.ub && F.spy(vol.t[2]) < vol.ub && F.xlp(vol.t[3]) < vol.ub;
 
-    if ((value > quantile(F, 1 - prob.trig) && position %in% c(-1, 0)) ||
-        (value > quantile(F, 0.45) && position == 1)) {
-    ## if ((prob > 1 - prob.trig && position %in% c(-1, 0)) || (prob > 0.45 && position == 1)) {
-        ## Short the portfolio
-        action <- rbind(action, c(t, -1));
-        if (position %in% c(0, -1)) {## Not in a position or in a short position
-            holding <- holding - c(1, -coef(model));
-            cash[t] <- cash[t-1] + sum(c(1, -coef(model)) * X[t, c(2,4,6)]);
-            position <- -1;
-        } else if (position == 1) {## In a long position
-            cash[t] <- cash[t-1] + sum(holding * X[t, c(2,4,6)]);
-            holding <- rep(0, 3);
-            position <- 0;
-        }
-    } else if ((value < quantile(F, prob.trig) && position %in% c(0, 1)) ||
-               (value < quantile(F, 0.55) && position == -1)) {
-        ## Long the portfolio
-        action <- rbind(action, c(t, 1));
-        if (position %in% c(0, 1)) {
-            ## Not in a position or in a long position
-            holding <- holding + c(1, -coef(model));
-            cash[t] <- cash[t-1] - sum(c(1, -coef(model)) * X[t, c(2,4,6)]);
-            position <- 1;
-        } else if (position == -1) {
-            ## In a short position
-            cash[t] <- cash[t-1] + sum(holding * X[t, c(2,4,6)]);
-            holding <- rep(0, 3);
-            position <- 0;
-        }
-    }
-    else {
-        cash[t] <- cash[t-1];
-    }
-    wealth[t] <- cash[t] + sum(holding * X[t, c(2,4,6)]);
+    ## if (!trading.allowed && position == 0) {
+    ##     cash[t] <- cash[t-1];
+    ##     wealth[t] <- wealth[t-1];
+    ##     next;
+    ## }
+
+    ## KO is leading
+    ## if (mean(deviations[period]/prices) > 3.0e-3) {
+    ##     holding <- holding + shares * (cash)
+    ## }
+    model <- lm(ko~pep+spy+xlp, data=prices);
+    deviations[t] <- X$ko[t] - predict(model, newdata=X[t, ], n.ahead=1);
+    shares <- c(1, -tail(coef(model), n=-1));
+    ratio.cal[t, ] <- shares;
+
+    ## value <- deviations[t];
+
+    ## F <- ecdf(residuals(model));
+
+    ## if (value > quantile(F, 1 - prob.trig)) {
+    ## ## if ((prob > 1 - prob.trig && position %in% c(-1, 0)) || (prob > 0.45 && position == 1)) {
+    ##     ## Short the portfolio
+    ##     action <- rbind(action, c(t, -1));
+    ##     cash[t] <- cash[t-1] + sum(shares * X[t, c(2,4,6)]);
+    ##     holding <- holding - shares;        
+    ## } else if (value < quantile(F, prob.trig)) {
+    ##     ## Long the portfolio
+    ##     action <- rbind(action, c(t, 1));
+    ##     holding <- holding + shares;
+    ##     cash[t] <- cash[t-1] - sum(shares * X[t, c(2,4,6)]);
+    ## } else {
+    ##     cash[t] <- cash[t-1];
+    ## }
+    ## wealth[t] <- cash[t] + sum(holding * X[t, c(2,4,6)]);
 }
+Y <- tail(deviations, n=-look.back);
+dY <- diff(Y);
+K <- length(dY);
 
+cor.1 <- lapply(30:K, FUN=function(i) acf(dY[(i-29):i], lag.max=1)$acf[2]);
+plot(1:length(cor.1), cor.1, type="l");
+abline(h=c(-2/sqrt(30), 2/sqrt(30)), col="#FF0000");
+
+plot(1:length(deviations), deviations, type="l");
+
+par(mfrow=c(3, 1));
+plot((look.back+1):n, ratio.cal[(look.back+1):n, 2], type="l");
+plot((look.back+1):n, ratio.cal[(look.back+1):n, 3], type="b");
+plot((look.back+1):n, ratio.cal[(look.back+1):n, 4], type="b");
+
+C <- ratio.cal[(look.back+1):n, ];
+dC <- apply(C, MARGIN=2, FUN=diff);
+dC.rel <- dC[, 2]/head(C[, 2], n=-1);
+I <- which(abs(dC.rel) < 1.0e-1) + look.back + 1;
+J <- setdiff(1:n, I);
+Z <- rep(NA, n);
+Z[I] <- deviations[I];
+Z[J] <- 0;
+
+par(mfrow=c(2, 1));
+plot(1:n, Z, "l");
+points(J, rep(0, length(J)), col="#FF0000");
+plot(1:n, ratio.cal[, 2], type="b");
+points(J, rep(0, length(J)), col="#FF0000");
+abline(h=seq(from=-0.3, to=0.4, by=0.1), col="#00FF00");
+
+plot(1:dim(dC)[1], dC.rel,
+     ## ylim=c(-1, 1),
+     type="b");
+
+F <- ecdf(dC.rel);
+den <- density(dC.rel);
+plot(den$x, den$y, type="l");
+
+
+
+J <-900:1500;
+plot(J, wealth[J], type="l");
 
 long.days <- which(action[, 2] == 1);
 short.days <- which(action[, 2] == -1);
--- OUstrategy.r
+++ OUstrategy.r
@@ -4,148 +4,83 @@ require(RMySQL);
 require("xts");
 require("fGarch");
 require("sde");
-
+require("zoo");
+require("TTR");
 
 database = dbConnect(MySQL(), user='sinbaski', password='q1w2e3r4',
                      dbname='market', host="localhost");
 
 
 stmt <- paste(
-    "select ko_daily.tm as tm,", 
+    "select ko_daily.tm as tm,",
     "ko_daily.closing as ko,",
-    "ko_daily.high - ko_daily.low ko_range,",
+    "pep_daily.closing as pep,",
     "spy_daily.closing as spy,",
-    "spy_daily.high - spy_daily.low as spy_range,",
-    "xlp_daily.closing as xlp,",
-    "xlp_daily.high - xlp_daily.low as xlp_range",
-    "from ko_daily join spy_daily join xlp_daily",
-    "on ko_daily.tm = spy_daily.tm and ko_daily.tm = xlp_daily.tm;"
+    "xlp_daily.closing as xlp",
+    "from ko_daily join pep_daily",
+    "join spy_daily",
+    "join xlp_daily",
+    "on ko_daily.tm = pep_daily.tm",
+    "and ko_daily.tm = spy_daily.tm",    
+    "and ko_daily.tm = xlp_daily.tm"    
 );
+
 results <- dbSendQuery(database, stmt);
 X <- fetch(results, n=-1);
 dbClearResult(results);
 dbDisconnect(database);
 
-dX <-as.data.frame(apply(X[,c(2,4,6)], MARGIN=2, FUN=diff));
+n <- dim(X)[1];
+p <- dim(X)[2]-1;
+look.back <- 60;
+ma.t <- 10;
+action <- {};
+prob.trig <- 0.05;
+holding <- matrix(0, nrow=n, ncol=p);
+cash <- matrix(NA, nrow=n, ncol=p); 
+wealth <- matrix(NA, nrow=n, ncol=p);
+purchase <- matrix(NA, nrow=n, ncol=p);
 
-## n1 <- dim(X)[1] %/% 3;
-## n2 <- dim(X)[1] - n1;
-## model <- lm(ko ~ spy + xlp, data=X[1:n1,]);
-## Y <- tail(X$ko, n=-n1) - as.matrix(X[(n1+1):n, 1:2]) %*% coef(model)[-1] - coef(model)[1];
-
-n <- dim(dX)[1];
-look.back <- 252;
-L <- 125;
-rebase <- seq(from=look.back+1, to=n, by=L);
-C <- matrix(NA, nrow=length(rebase), ncol=2);
-Y <- rep(NA, n-look.back);
-idx <- 1;
-for (i in 1:length(rebase)) {
-    I <- (rebase[i]-look.back):(rebase[i]-1);
-    # dZ <- dX[I, ]/X[I+1, c(3,5,7)]
-    model <- lm(ko~spy+xlp-1, data=dX[I, ]);
-    C[i, ] <- coef(model);
-    ## R <- dX[rebase[i]:(rebase[i] + L - 1), 1] - as.matrix(dX[rebase[i]:(rebase[i] + L - 1), c(2,3)]) %*% C[i, ];
-    Y[idx:(idx+L-1)] <- X[rebase[i]:(rebase[i]+L-1), 2] - as.matrix(X[rebase[i]:(rebase[i]+L-1), c(4,6)]) %*% coef(model);
-    idx <- idx + L;
+deviations <- rep(NA, n);
+ratio.hold <- matrix(NA, nrow=n, ncol=p);
+ratio.cal <- matrix(NA, nrow=n, ncol=p);
+for (t in (look.back + 1):n) {
+    period <- (t-look.back):(t-1);
+    prices <- X[period, ];
+    model <- lm(ko~pep+spy+xlp, data=prices);
+    deviations[t] <- X$ko[t] - predict(model, newdata=X[t, ], n.ahead=1);
+    shares <- c(1, -tail(coef(model), n=-1));
+    ratio.cal[t, ] <- shares;
     
-    ## fit an Ornstein-Uhlenbeck process to the residuals
-    ## lik.fun <- function(mu, theta, sigma) {
-    ##     -sum(dcOU(x=tail(Y, n=-1),
-    ##               Dt=1,
-    ##               x0=head(Y, n=-1),
-    ##               theta=c(theta*mu, theta, sigma),
-    ##               log=TRUE
-    ##               ));
-    ## };
-    ## sigma <- sd(R);
-    ## fit <- optim(par=c(0.001, sigma),
-    ##              fn=function(param) lik.fun(1, param[1], param[2])
-    ##              ## method="L-BFGS-B"
-    ##              );
+    value <- deviations[t];
+    F <- ecdf(residuals(model));
 
-
-    ## C.cov[,,i] <- cov(Z);
-    ## C.cor[,,i] <- cor(Z);
-
-    ## model <- lm(ko ~ spy + xlp);
-    ## R <- diff(log(X[i:(i + L - 1), 1])) -
-    ##     cbind(diff(log(X[i:(i+L-1), 2])),
-    ##           diff(log(X[i:(i+L-1), 3]))) %*% coef(model)[-1] - coef(model)[1];
-    ## Y[idx : (idx + L - 1)] = c(1, exp(cumsum(R)));
-    ## idx <- idx + L;
+    inc.neutral <- rep(NA, p);
+    if (holding[t-1, 1] > 0) {## currently long on KO
+        inc.neutral[-1] <- shares[-1] * holding[t-1, 1] - holding[t-1, -1];
+        inc.neutral[1] <- 0;
+    } else if (holding[t-1, 1] == 0) {## currently neutral on KO
+        inc.neutral <- rep(0, p);
+    } else {## currently short on KO
+        inc.neutral[-1] <- shares[-1] * holding[t-1, 1] - holding[t-1, -1];
+        inc.neutral[1] <- 0;
+    }
     
-    ## OU process of the prices
-    ## ko <- X$ko[I];
-    ## spy <- X$spy[I];
-    ## xlp <- X$xlp[I];
-    ## model <- lm(ko ~ spy + xlp);
-    ## C[, idx] <- coef(model);
-
-    ## J <- i:(i+L-1);
-    ## Y[J-look.back] <- X$ko[J] - as.matrix(X[J, 1:2]) %*% C[-1, idx] - C[1, idx];
-
-    ## idx <- idx + 1;
-
-
-    ## beta <- coef(model)[-1];
-    ## intercept <- coef(model)[1];
-    ## R <- residuals(model);
-
-    
-    ## Y <- X$ko[J] - cbind(X$spy[J], X$xlp[J]) %*% beta;
-    ## ptfl[J] <- Y;
+    if (holding[t-1, 1] > 0) {
+        if (value < quantile(F, prob.trig)) {
+            ## Long KO
+            purchase[t, ] <- inc.neutral + shares;
+        } else if (value > quantile(F, 0.5 - prob.trig) && value < quantile(F, 1 - prob.trig)) {
+            ## dump the portfolio, ignoring the neutrality increment
+            purchase[t, ] <- -holding[t-1, ];
+        } else if (value > quantile(F, 0.5 - prob.trig) && value < quantile(F, 1 - prob.trig)) {
+            purchase[t, ] <- -holding[t-1, ] - shares;
+        }
+    } else if (holding[t-1, 1] < 0) {    ## currently short on KO
+        if (value > quantile(F, 1 - prob.trig)) {
+            ## short even further
+            purchase[t, ] <- inc.neutral - shares;
+        }
+    }
+    holding[t, ] <- holding[t-1, ] + purchase[t, ];    
 }
-plot(1:length(Y), Y, t="l");
-abline(v=seq(from=20, to=length(Y), by=20), col="#0000FF");
-abline(h=0, col="#FF0000");
-plot(1:dim(C)[1], C[, 1], type='l');
-
-plot(1:dim)
-par(mfrow=c(2,3));
-plot(1:dim(C.cov)[3], C.cov[1,1,], type="l");
-plot(1:dim(C.cov)[3], C.cov[2,2,], type="l");
-plot(1:dim(C.cov)[3], C.cov[3,3,], type="l");
-plot(1:dim(C.cov)[3], C.cov[1,2,], type="l");
-plot(1:dim(C.cov)[3], C.cov[1,3,], type="l");
-plot(1:dim(C.cov)[3], C.cov[2,3,], type="l");
-
-## plot((L+1):length(X$ko), tail(X$ko, n=-L), type="l");
-## lines(1:length(ptfl), ptfl, col="#0000FF");
-plot(1:length(ptfl), ptfl, col="#0000FF");
-
-## series <- xts(ptfl[2:(n+1)], order.by=as.Date(X$tm));
-## plot(series);
-plot(ptfl, type="l",
-     ylab="portfolio price",
-     xlab="time"
-     );
-rebase <- (1:periods)*L;
-## abline(v=rebase, col='#00FF00');
-abline(h=1, col='#FF0000')
-
-abline(h = -1:5, v = -2:3, col = "red", lty = 3)
-## W <- rep(NA, 2);
-## W[2] <- 1/(1 - coef(model)[1]);
-## W[1] <- 1 - W[2];
-
-## eps <- residuals(model);
-## Y <- exp(cumsum(eps));
-## series <- xts(Y, order.by=as.Date(X$tm));
-## plot(series);
-## abline(h=0, col='#FF0000');
-
-
-
-## Y.ma <- rollmean(Y, )
-    
-
-
-
-## dev <- xts(series$dev, order.by=as.Date(series$day));
-## plot(dev);
-## abline(h=0, col='#FF0000');
-
-
-
-
