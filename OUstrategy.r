bcb6bcb32fa181ef7097c67c2ad7f7ace25d292e
Xiaolei Xie <xiaolei.xie@atenvia.com>
2017-12-04 22:51:55 +0000

    Revised the OOU strategy.

--- OUstrategy.r
+++ OUstrategy.r
@@ -5,21 +5,37 @@ require(ts);
 require("xts");
 require("fGarch");
 require("sde");
-require("zoo");
-require("TTR");
-source("libeix.r");
+source("~/cake/libeix.r");
 
+## Number of shares to buy/short. Always positive.
+pstn.sz.shape <- function(x, dist)
+{
+    lower <- if (x < 0) -Inf else x;
+    upper <- if (x < 0) x else Inf;
+
+    loss <- Inf;
+    if (dist$dist == "norm") {
+        loss <- integrate(f=function(x) x * dnorm(x=x, mean=dist$mu, sd=dist$sig),
+                          lower=lower, upper=upper
+                          );
+    } else if (dist$dist == "t") {
+        loss <- integrate(f=function(x) x * dt(x=x - dist$mu, df=dist$df),
+                          lower=lower, upper=upper
+                          );
+    }
+    return(x/loss$value);
+}
 
 ## Assume the deviations are normally distributed
 ## Trade size increases exponentially with the deviation
 ## Determine the rate of exponential growth such that the
 ## expected trade size is a fixed number for a given threshold.
-expected.trade.size <- function(lambda, sig, threshold)
-{
-    -exp(lambda^2 * sig^2 / 2) * (
-        erf((threshold - lambda * sig^2)/sqrt(2 * sig)) - 1
-    )/2
-}
+## expected.pos.sz <- function(lambda, sig, threshold)
+## {
+##     -exp(lambda^2 * sig^2 / 2) * (
+##         erf((threshold - lambda * sig^2)/sqrt(2 * sig)) - 1
+##     )/2
+## }
 
 database = dbConnect(MySQL(), user='sinbaski', password='q1w2e3r4',
                      dbname='market', host="localhost");
@@ -44,7 +60,7 @@ data <- fetch(results, n=-1);
 dbClearResult(results);
 dbDisconnect(database);
 
-X <- data[, -1];
+X <- as.matrix(data[, -1]);
 tm <- data[, 1];
 rm(data);
 
@@ -56,133 +72,110 @@ ret[-1, ] <- apply(log(X), MARGIN=2, FUN=diff);
 
 look.back <- 60;
 prob.trig <- 0.05;
-## free cash
-cash <- rep(0, n); 
-wealth <- rep(0, nrow=n, ncol=p);
+## expected shortfall of 1 share of the portfolio
+ES <- rep(0, n);
 deviations <- rep(NA, n);
+## Initial capital
+cap.init <- 1;
+## free cash
+cash <- rep(NA, n); 
+wealth <- rep(NA, n);
 ## KO, reserved cash, PEP, SPY, XLP
 holding <- matrix(0, nrow=n, ncol=p+1);
 purchase <- matrix(0, nrow=n, ncol=p+1);
 ratio.cal <- matrix(NA, nrow=n, ncol=p+1);
-size.target <- 1;
-ccc.garch <- vector("list", p);
-confidence.risk <- 0.95;
-limit.risk <- 5.0e-2;
-allocation <- 1;
-for (t in (2*look.back + 1):n) {
+prob.es <- 0.05;
+target.risk <- 0.05;
+for (t in (look.back + 1):n) {
+    if (is.na(wealth[t-1])) {
+        wealth[t-1] = cap.init;
+        cash[t-1] <- cap.init;
+    }
+    
     period <- (t-look.back):(t-1);
     model <- lm(ko~pep+spy+xlp, data=X[period, ]);
 
     R <- residuals(model);
-    sig <- sd(R);
-    F <- ecdf(R);
-    ## Find the curve of trade size
-    ub <- 2;
-    lb <- 0.5;
-    threshold <- -quantile(F, prob.trig);
-    while (expected.size.target(ub, sig, threshold) < size.target)
-        ub <- ub * 2;
-    while (expected.size.target(lb, sig, threshold) > size.target)
-        lb <- lb / 2;
-    lambda <- uniroot(function(x) expected.trade.size(x, sig=sig, threshold=threshold) - size.target, c(lb, ub))$root;
-    
-    shares <- c(1, -coef(model));
+    F.R <- fit.dist(R);
+
+    shares <- c(1, -coef(model)[-1], -coef(model)[1]);
     ratio.cal[t, ] <- shares;
     deviations[t] <- X[t, 1] - predict(model, newdata=X[t, ], n.ahead=1);
     
     value <- deviations[t];
-    size.cur <- exp(lambda * value);
-
     ## QRM: CCC-GARCH with diagonal-matrix coefficients
     inno <- matrix(NA, nrow=look.back, ncol=p);
     vol.pred <- rep(NA, p);
     mean.pred <- rep(NA, p);
     for (i in 1:p) {
-        ccc.garch[[i]] <- garchFit(formula=~garch(1,1),
-                                   ret[period+1, i],
-                                   include.mean=TRUE,
-                                   trace=FALSE);
-        inno[, i] <- ret[period + 1, i]/ccc.garch[[i]]@sigma.t;
-        vol.pred[i] <- predict(ccc.garch[[i]], n.ahead=1)$standardDeviation;
-        mean.pred[i] <- coef(ccc.garch[[i]])[1];
+        mdl <- fit.garch(ret[period+1, i]);
+        inno[, i] <- mdl$inno;
+        vol.pred[i] <- sum(mdl$alpha * mdl$r.t^2) + sum(mdl$beta * mdl$sigma.t^2) + mdl$omega;
+        mean.pred[i] <- mdl$mu;
     }
     C <- cov(inno);
 
     ## Now compute the portfolio distribution parameters of day t+1
-
     E <- eigen(C);
     A <- E$vectors %*% diag(sqrt(E$values));
-    ## W <- abs(holding[t-1, -2]) * X[t-1, ];
-    W <- abs(shares) * X[t, ];
-    W <- W/sum(W);
+    ## Take the current price of KO as the unit of curreny.
+    W <- shares * c(X[t, ], 1) / X[t, 1];
+        
+    sd.ptfl <- sqrt(sum(((W[1:p] * vol.pred) %*% A)^2));
+    mean.ptfl <- sum(mean.pred * W[1:p]);
+    ES[t] <- expected.shortfall(c(mean.ptfl, sd.ptfl), alpha=prob.es);
+    ## conditional expected loss for one share of the portfolio
+    ES[t] <- ES[t] * X[t, 1];
 
-    sd.ptfl <- sqrt(sum(((W * vol.pred) %*% A)^2));
-    mean.ptfl <- sum(mean.pred * W);
-    VaR <- qnorm(p=confidence.risk, mean=mean.ptfl, sd=sd.ptfl);
-    ## expected shortfall
-    ES <- integrate(f=function(x) x * dnorm(x, mean=mean.ptfl, sd=sd.ptfl), lower=VaR, upper=Inf)$value/(1 - confidence.risk);
+    ## When the deviation is at F^{-1}(pnrom(2)),
+    ## our total ES in percentage should be target.risk
+    wealth[t] <- sum(X[t, ] * holding[t-1, 1:p]) + holding[t-1, p+1] + cash[t-1];
+    pstn.sz.anchor <- target.risk * wealth[t]/ES[t];
+    if (F.R$dist == "norm") {
+        Q.2s <- F.R$mu + 2 * F.R$sig;
+        Q.1s <- F.R$mu + F.R$sig;
+    } else if (F.R$dist == "t") {
+        Q.2s <- qt(p=pnorm(2), df=F.R$df) + F.R$mu;
+        Q.1s <- qt(p=pnorm(1), df=F.R$df) + F.R$mu;
+    }
+    pstn.sz.const <- pstn.sz.anchor/pstn.sz.shape(Q.2s, F.R);
+    pstn.sz.min <- pstn.sz.const * pstn.sz.shape(Q.1s, F.R);
+    pstn.sz <- pstn.sz.const * pstn.sz.shape(value, F.R);
+
+
+    ## As long as we don't exceed the risk limit, adjust the position without further
+    ## cash commitment.
+    ## Otherwise align the postion to neutrality and adjust its size to the risk limit.
     
     ## adjustment to neutrality without changing KO shares
     ## only use this adjustment if a neutrality adjustment with zero cash flow is impossible
-    inc.neutral <- rep(NA, p+1);
-    if (holding[t-1, 1] != 0) {
-        inc.neutral[-1] <- shares[-1] * holding[t-1, 1] - holding[t-1, -1];
-        inc.neutral[1] <- 0;
-    } else {## currently neutral on KO
-        inc.neutral <- rep(0, p+1);
-    }    
-
-    if (holding[t-1, 1] > 0) {
-        if (value < quantile(F, prob.trig)) {
-            ## bottom: Long KO
-            purchase[t, ] <- inc.neutral + shares;
-        } else if (value > quantile(F, 0.5 - prob.trig) && value <= quantile(F, 1 - prob.trig)) {
-            ## middle and upper: clear the portfolio, ignoring the neutrality increment
-            purchase[t, ] <- -holding[t-1, ];
-        } else if (value > quantile(F, 1 - prob.trig)) {
-            ## top: clear the portfolio and take the short KO position
-            purchase[t, ] <- -holding[t-1, ] - shares;
-        } else { ## value > quantile(F, prob.trig) && value < quantile(F, 0.5 - prob.trig)
-            ## lower: adjust the portfolio
-            ## NOTE: value < 0
-            intercept <- sum(X[t, -1] * tail(ratio.cal[t-1, ], n=-2)) - ratio.cal[t, 2] + ratio.cal[t-1, 2];
-            if (sign(intercept) < 0) {
-                k <- intercept / value;
-                purchase[t, ] <- k * shares - holding[t-1, ];
-            } else if (sign(intercept) >= 0) {
-                ## Adjust to market neutrality
-                purchase[t, ] <- inc.neutral;
+    ## inc.neutral <- rep(NA, p+1);
+    ## if (holding[t-1, 1] != 0) {
+    ##     inc.neutral[-1] <- shares[-1] * holding[t-1, 1] - holding[t-1, -1];
+    ##     inc.neutral[1] <- 0;
+    ## } else {## currently neutral on KO
+    ##     inc.neutral <- rep(0, p+1);
+    ## }
+    if (holding[t-1, 1] == 0 && pstn.sz > pstn.sz.min) { ## cash-only
+        purchase[t, ] <- shares * pstn.sz * sign(-value);
+    } else if (holding[t-1, 1] != 0) {
+        pm <- sign(holding[t-1, 1]);
+        if (pm != sign(value)) {
+            b <- sum(holding[t-1, ] * X[t, ]);
+            a <- sum(shares * X[t, ]);
+            if (sign(b) == sign(b) && b/a < pstn.sz) {
+                ## We can adjust without further cash commitment
+                purchase[t, ] <- pm * shares * (b/a) - holding[t-1, ];
+            } else if {
+                ## Either zero-cash adjustment is impossible or the risk limit is exceeded
+                ## Adjust to the recommended position size
+                purchase[t, ] <- pm * pstn.sz * shares - holding[t-1, ];
             }
-        }
-    } else if (holding[t-1, 1] < 0) {    ## currently short on KO
-        if (value > quantile(F, 1 - prob.trig)) {
-            ## top: short KO
-            purchase[t, ] <- inc.neutral - shares;
-        } else if (value < quantile(F, 0.5 + prob.trig) && value > quantile(F, prob.trig)) {
-            ## middle and lower: clear the portfolio, ignoring the neutrality increment
-            purchase[t, ] <- -holding[t-1, ];
-        } else if (value < quantile(F, prob.trig)) {
-            ## bottom: clear the portfolio and take the long KO position
-            purchase[t, ] <- -holding[t-1, ] + shares;
-        } else { ## value > quantile(F, prob.trig) && value > quantile(F, 0.5 + prob.trig)
-            ## upper: adjust the portfolio
-            ## NOTE: value > 0
-            intercept <- sum(X[t, -1] * tail(ratio.cal[t-1, ], n=-2)) - ratio.cal[t, 2] + ratio.cal[t-1, 2];
-            if (sign(intercept) > 0) {
-                k <- intercept / value;
-                purchase[t, ] <- -k * shares - holding[t-1, ];
-            } else if (sign(intercept) <= 0) {
-                ## Adjust to market neutrality
-                purchase[t, ] <- inc.neutral;
+        } else { ## pm == sign(value) The portfolio has shifted to the other side
+            purchase[t, ] <- -holding[t-1, 1];
+            if (pstn.sz > pstn.sz.min) {
+                purchase[t, ] <- purchase[t, ] - sign(value) * pstn.sz * shares;
             }
-        }
-    } else { ## cash-only
-        if (value < quantile(F, prob.trig)) {
-            ## bottom: Long KO
-            purchase[t, ] <- shares;
-        } else if (value > quantile(F, 1 - prob.trig)) {
-            ## top: short KO
-            purchase[t, ] <- -shares;
         }
     }
     holding[t, ] <- holding[t-1, ] + purchase[t, ];
--- libeix.r
+++ libeix.r
@@ -1,91 +1,134 @@
 erf <- function(x) 2 * pnorm(x * sqrt(2)) - 1;
 
-fit.OU <- function(ts)
+fit.dist <- function(data)
 {
-    minus.log.lik <- function(theta, sig)
-    {
-        -sum(dcOU(x=tail(ts, n=-1), Dt=1, x0=head(ts, n=-1), theta=c(0, theta, sig), log=TRUE))
+    fit <- list(bic=Inf);
+
+    mu <- mean(data);
+    sig <- sd(data);
+
+    ## First try normal distribution
+    params <- optim(
+        fn=function(arg) -sum(dnorm(x=data, mean=arg[1], sd=arg[2], log=TRUE)),
+        par=c(mu, sig),
+        lower=c(min(data), sig/2),
+        upper=c(max(data), sig*2),
+        method="L-BFGS-B"
+    );
+    if (params$convergence == 0) {
+        fit <- list(
+            dist="norm",
+            bic=2*log(length(data)) + 2 * params$value,
+            mu=params$par[1],
+            sig=params$par[2]
+            );
     }
-    theta.init <- -log(mean(tail(ts, n=-1)/head(ts, n=-1)));
-    sig.init <- sd(diff(R));
-    params <- optim(par=c(theta.init, sig.init),
-                    fn=function(arg) minus.log.lik(arg[1], arg[2]),
-                    method="L-BFGS-B",
-                    lower=c(theta.init/10, sig.init/10),
-                    upper=c(theta.init*2, sig.init*2),
-                    );
-    return(params$par);
+
+    ## non-central t distribution
+    nu <- 2*sig^2/(sig^2 - 1);
+    df <- if (nu > 0) nu else 1.5;
+    params <- optim(
+        fn=function(arg) -sum(dt(x=data - arg[1], df=arg[2], log=TRUE)),
+        par=c(mu, df),
+        lower=c(min(data), 1),
+        upper=c(max(data), 10),
+        method="L-BFGS-B"
+    );
+    if (params$convergence == 0) {
+        bic <- 2*log(length(data)) + 2 * params$value;
+        if (fit$bic > bic) {
+            fit <- list(
+                dist="t",
+                bic=bic,
+                mu=params$par[1],
+                df=params$par[2]
+            );
+        }
+    }
+    return(fit);
 }
 
+fit.garch <- function(data, max.order=c(3,3))
+{
+    score <- Inf;
+    result <- {};
+    bics <- matrix(NA, nrow=max.order[1], ncol=max.order[2]);
+    for (i in 1:max.order[1]) {
+        for (j in 1:max.order[2]) {
+            score <-  tryCatch(
+                expr = {
+                    mdl <- garchFit(as.formula(sprintf("~garch(%d, %d)", i, j)),
+                                    data=data,
+                                    trace=FALSE);
+                    bics[i, j] <- mdl@fit$ics[2];
+                    if (score > mdl@fit$ics[2]) {
+                        par <- coef(mdl);
+                        result <- list(mu=par[1], omega=par[2],
+                                       alpha=par[3:(3 + i - 1)],
+                                       beta=par[(3 + i):length(par)],
+                                       sigma.t=tail(mdl@sigma.t, n=j),
+                                       r.t=tail(data, n=i),
+                                       inno=(data - par[1])/mdl@sigma.t,
+                                       bic=bics[i, j]
+                                       );
+                    }
+                    min(score, bics[i, j]);
+                }, error = function(e) {
+                    score;
+                }
+            );
+        }
+    }
+    return(result);
+}
 
-
-
-## compute.volatility <- function(symbol)
+## fit.OU <- function(ts)
 ## {
-##     database = dbConnect(MySQL(), user='sinbaski', password='q1w2e3r4',
-##                          dbname='market', host="localhost");
-
-##     stmt <- sprintf(
-##         paste(
-##         "create table %s_vol (",
-##         "tm date primary key,",
-##         "realized double,",
-##         "highlow double",
-##         ");"
-##         ), symbol
-##     );
-##     results <- dbSendQuery(database, stmt);    
-
-##     stmt <- sprintf(
-##         "select distinct(date(tm)) as t from %s_1min;", symbol
-##     );
-##     results <- dbSendQuery(database, stmt);
-##     days <- fetch(results, n=-1)$t;
-##     dbClearResult(results);
-
-##     vol.realized <- rep(NA, length(days));
-##     vol.range <- rep(NA, length(days));
-##     for (t in 2:length(days)) {
-##         stmt <- sprintf(
-##             "select closing from %s_1min where date(tm) = \"%s\" order by tm;", symbol, days[t]
-##         );
-##         results <- dbSendQuery(database, stmt);
-##         X <- fetch(results, n=-1);
-##         dbClearResult(results);
-##         ret <- diff(log(X$closing));
-##         vol.realized[t] <- sqrt(sum(ret^2));
-
-##         stmt <- sprintf(
-##             "select max(high) as high, min(low) as low from %s_1min where date(tm) = \"%s\";", symbol, days[t]
-##         );
-##         results <- dbSendQuery(database, stmt);
-##         X <- fetch(results, n=-1);
-##         dbClearResult(results);
-##         high <- X$high;
-##         low <- X$low;
-
-##         stmt <- sprintf(
-##             paste(
-##                 "select closing from %s_1min where tm = ",
-##                 "(select tm from %s_1min where date(tm) < \"%s\" order by tm desc limit 1);"
-##             ), symbol, symbol, days[t]
-##         );
-##         results <- dbSendQuery(database, stmt);
-##         closing <- fetch(results, n=-1)$closing;
-##         dbClearResult(results);
-
-##         vol.range[t] <- log(max(high, closing)) - log(min(low, closing));
+##     minus.log.lik <- function(theta, sig)
+##     {
+##         -sum(dcOU(x=tail(ts, n=-1), Dt=1, x0=head(ts, n=-1), theta=c(0, theta, sig), log=TRUE))
 ##     }
-##     vol <- cbind(vol.realized[-1], vol.range[-1]);
-##     write.table(vol, sprintf("/tmp/%s_vol.txt", symbol), sep=",",
-##                 row.names=FALSE, col.names=FALSE);
-##     stmt <- sprintf(
-##         paste(
-##             "load data infile '/tmp/%s_vol.txt' into table %s_vol",
-##             "fields terminated by \",\";"
-##         ), symbol, symbol
+##     tmp <- tail(ts, n=-1)/head(ts, n=-1);
+##     theta.init <- -log(mean(tmp[tmp > 0]));
+##     if (theta.init <= 0) return list(fitted=FALSE);
+##     sig.init <- sd(diff(R));
+    
+##     fit <- trycatch(
+##         expr={
+##             params <- optim(par=c(theta.init, sig.init),
+##                             fn=function(arg) minus.log.lik(arg[1], arg[2]),
+##                             method="L-BFGS-B",
+##                             lower=c(theta.init/10, sig.init/10),
+##                             upper=c(theta.init*4, sig.init*4),
+##                             control=list(factr=0.001, maxit=500)
+##                             );
+##             if (params$convergence != 0) list(fitted=FALSE) else list(fitted=TRUE, params$par);
+##         }, error=function(e) {
+##             list(fitted=FALSE);
+##         }
 ##     );
-##     results <- dbSendQuery(database, stmt);
-##     dbDisconnect(database);
-##     return(list(realized=vol.realized, range=vol.range));
+##     return(fit);
 ## }
+
+cummax <- function(X)
+{
+    n <- length(X);
+    Y <- rep(NA, n);
+    val <- -Inf;
+    for (i in 1:n) {
+        if (X[i] > val) {
+            val <- X[i];
+        }
+        Y[i] <- val;
+    }
+    return(Y);
+}
+
+## Integrate the lower-tail of the return distribution. Always positive
+## params: parameters of the return distribution
+expected.shortfall <- function(params, alpha)
+{
+    integral <- integrate(f=function(x) qnorm(p=x, mean=params[1], sd=params[2]), lower=0, upper=alpha);
+    return(-integral$value/alpha);
+}
+
