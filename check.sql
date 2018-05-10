3206af89432d5fb138199c80831bf360b1cdb19e
Xie Xiaolei <xie.xiaolei@gmail.com>
2018-05-10 21:21:48 +0100

    Added a C++ program for pricing European call options using Black-Schole.

--- check.sql
+++ check.sql
@@ -1,15 +1,15 @@
 drop view if exists monthly_values;
 create view monthly_values as
 select year(tm) as yr, month(tm) as mn, avg(worth) as worth
-from master_jjg_weat_soyb_trade_log
+from NeonV2_uup_fxe_fxy_trade_log
 group by yr, mn order by yr, mn;
 
 drop view if exists trades;
 create view trades as
 select A.tm, A.worth/B.worth - 1 as ret
-from master_jjg_weat_soyb_trade_log as A join master_jjg_weat_soyb_trade_log as B
+from NeonV2_uup_fxe_fxy_trade_log as A join NeonV2_uup_fxe_fxy_trade_log as B
 on A.tm = (
-   select tm from master_jjg_weat_soyb_trade_log where tm > B.tm
+   select tm from NeonV2_uup_fxe_fxy_trade_log where tm > B.tm
    and worth != B.worth limit 1
 );
 
@@ -19,36 +19,54 @@ select A.tm, A.worth as worth,
 max(B.worth) as sup,
 1 - A.worth/max(B.worth) as DD
 from
-master_jjg_weat_soyb_trade_log as A join
-master_jjg_weat_soyb_trade_log as B on A.tm >= B.tm
+NeonV2_uup_fxe_fxy_trade_log as A join
+NeonV2_uup_fxe_fxy_trade_log as B on A.tm >= B.tm
 group by A.tm
 );
 
-select avg(ret)/std(ret) as daily_sharpe from trades
-where tm < '2016-06-23';
+-- drop view if exists exposure;
+-- create view exposure as 
+-- select tm, (abs(uup_q) + abs(fxe_q) + abs(fxy_q)) as exposed
+-- from NeonV2_uup_fxe_fxy_trade_log
+-- where tm <= '2016-06-23';
 
--- select year(tm) as yr, month(tm) as mn, format(avg(ret)/std(ret), 3) as daily_sharpe
--- from trades group by yr, mn order by yr, mn;
+drop view if exists turnover;
+create view turnover as
+select B.tm,
+-- (abs(B.uup_q - A.uup_q) + abs(B.fxe_q - A.fxe_q) + abs(B.fxy_q - A.fxy_q))/(abs(A.uup_q) + abs(A.fxe_q) + abs(A.fxy_q))
+(abs(B.uup_q - A.uup_q) + abs(B.fxe_q - A.fxe_q) + abs(B.fxy_q - A.fxy_q))/A.worth
+as ratio
+from NeonV2_uup_fxe_fxy_trade_log as A join NeonV2_uup_fxe_fxy_trade_log as B
+on B.tm = (
+   select tm from NeonV2_uup_fxe_fxy_trade_log where tm > A.tm limit 1
+)
+where A.worth != B.worth;
+-- and B.tm < '2016-06-23';
 
+select avg(ratio) from turnover;
 
-select avg(ret)/std(ret) as monthly_sharpe from (
+select avg(ret) as R, avg(ret)/std(ret) as daily_sharpe from trades;
+-- where tm < '2016-06-23';
+
+select avg(ret) as R, avg(ret)/std(ret) as monthly_sharpe from (
 select A.yr, A.mn, A.worth/B.worth - 1 as ret
 from monthly_values as A join monthly_values as B
 on
 A.yr = B.yr and A.mn = B.mn + 1
 or
 A.yr = B.yr + 1 and A.mn = 1 and B.mn = 12
-where A.yr < 2016 or A.yr = 2016 and A.mn < 6
+-- where A.yr < 2016 or A.yr = 2016 and A.mn < 6
 ) as tbl;
 
 
 select max(DD) from
 (select * from drawdowns
-where tm < '2016-06-23'
+-- where tm < '2016-06-23'
 ) as tbl;
 
 select avg(exposure) from (
-       select tm, (abs(jjg_q) + abs(weat_q) + abs(soyb_q))/worth as exposure
-       from master_jjg_weat_soyb_trade_log
-       where tm < '2016-06-23'
+       select tm, (abs(uup_q) + abs(fxe_q) + abs(fxy_q))/worth as exposure
+       from NeonV2_uup_fxe_fxy_trade_log
+--       where tm < '2016-06-23'
 ) as tbl;
+
--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,12 +1,136 @@
-#include <RInside.h>
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <string>
+#include <random>
+#include <algorithm>
+#include <mysql.h>
+#include <gsl/gsl_cdf.h>
+#include <numeric>
+
 
 using namespace std;
+random_device randev;
+
+// #include <RInside.h>
+
+// int main(int argc, char *argv[])
+// {
+//     RInside R(argc, argv);
+//     R["txt"] = "Hello World!\n";
+//     R.parseEvalQ("cat(txt)");
+//     return 0;
+// }
+
+class EuropeanCallOption
+{
+public:
+    static double fair_price(unsigned int T, double K, double r, const vector<double> &prices);
+};
+
+double EuropeanCallOption::fair_price(
+    unsigned int T, double K, double r, const vector<double> &prices)
+{
+    vector<double> S(prices);
+    transform(S.begin(), S.end(), S.begin(),
+	      [](double x) {return log(x);});
+    adjacent_difference(S.begin(), S.end(), S.begin());
+    S.erase(S.begin());
+    size_t n = S.size();
+    double avg = accumulate(
+	S.begin(), S.end(), (double)0,
+	[=](double y, double x) {
+	    return y + x/n;
+	});
+    double sigma = sqrt(accumulate(
+	S.begin(), S.end(), (double)0,
+	[=](double y, double x) {
+	    return y + (x - avg)*(x - avg)/(n-1);
+	}));
+    double d[2];
+    unsigned int t = prices.size();
+    d[0] = log(prices.back()/K) + (r + sigma * sigma/2) * (T - t);
+    d[0] /= sigma * sqrt(T - t);
+    d[1] = d[0] - sigma * sqrt(T - t);
+    return gsl_cdf_ugaussian_P(d[0]) * prices.back() -
+      gsl_cdf_ugaussian_P(d[1]) * K * exp(-r * (T - prices.size()));
+}
+
+class BrownianMotion
+{
+public:
+    static vector<double> simulate(unsigned int n);
+};
+
+class GeoBrownianMotion
+{
+public:
+    static vector<double> simulate(unsigned int n, double mu, double sigma);
+};
+
+vector<double> BrownianMotion::simulate(unsigned int n)
+{
+    normal_distribution<double> dist(0, 1);
+    vector<double> V(n);
+    generate(V.begin(), V.end(),
+    	     [&]()->double {
+    		 dist(randev);
+    	     });
+    // for_each(V.begin(), V.end(), [](double x) {printf("%e ", x);});
+    // cout << endl;
+    partial_sum(V.begin(), V.end(), V.begin());
+    return V;
+}
+
+vector<double> GeoBrownianMotion::simulate(
+    unsigned int n, double mu, double sigma)
+{
+    vector<double> B = BrownianMotion::simulate(n);
+    vector<unsigned int> tm(n);
+    unsigned int i = 1;
+    generate(tm.begin(), tm.end(), [&i]() -> double {
+	    return i++;
+	});
+    transform(
+	B.begin(), B.end(), tm.begin(), B.begin(),
+	[=](double x, unsigned int i) {
+	    return exp(sigma * x + (mu - sigma * sigma/2) * i);
+	});
+    B.insert(B.begin(), 1);
+    return B;
+}
+
 
 int main(int argc, char *argv[])
 {
-    RInside R(argc, argv);
-    R["txt"] = "Hello World!\n";
-    R.parseEvalQ("cat(txt)");
+    MYSQL *conn;
+    conn = mysql_init(NULL);
+    mysql_real_connect(conn, "127.0.0.1", "sinbaski", "q1w2e3r4", "market",
+		       0, NULL, 0);
+    int err = mysql_query(
+	conn,
+	"select closing from spy_daily order by tm desc limit 252"
+	);
+    if (err) {
+	cout <<  mysql_error(conn) << endl;
+	return 0;
+    }
+    MYSQL_RES *res = mysql_store_result(conn);
+    // vector<double> S(GeoBrownianMotion::simulate(504, 5.0e-4, 1.0e-3));
+    vector<double> S(mysql_num_rows(res));
+    generate(S.rbegin(), S.rend(), [res]() -> double {
+	    MYSQL_ROW row = mysql_fetch_row(res);
+	    return stod(row[0]);
+	});
+    mysql_free_result(res);
+    mysql_close(conn);
+    double interest_rate = 1.0e-4;
+    double s = EuropeanCallOption::fair_price(
+    	S.size() + 10, S.back() * 1.05, interest_rate, S
+    	);
+    cout << s << endl;
+    
     return 0;
 }
 
